10d2ba8c5eb5cc87fb32fdcb888f3d86d2082b26
Kyle Kowal <kkowalnotb@gmailcom>
2020-07-26 21:53:58 -0400

    Bootstrap row

--- script.js
+++ script.js
@@ -14,4 +14,10 @@ $(document).ready(function() {
     
             for (let i = 0; i < 12; i++) {
                 
-    
+    // Bootstrap Row
+    const rowDiv = $("<div>").addClass("row").attr("id", `row${i}`);
+
+    // Hour display div
+    const hourDiv = $("<div>").addClass("col-1 hour time-block d-flex align-items-center justify-content-center").text(date.format("H a")).attr("id", `hour${i}`);
+
+
